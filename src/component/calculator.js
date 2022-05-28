47587e77d421fce90567ea4d3730e5af985e5561
kinginthenorthcodez <kinginthenorthcodez@gmail.com>
2022-05-28 13:04:41 +0530

    fix Big js dependecy

--- src/component/calculator.js
+++ src/component/calculator.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import calculate from '../logic/calculate';
 
 class KeypadUI extends React.Component {
   constructor(props) {
@@ -7,19 +8,27 @@ class KeypadUI extends React.Component {
   }
 
   render() {
-    const total = 0;
+    // const total = 0;
+    const [displayed, setDisplayed] = useState({ total: 0, next: null, operation: null });
+    const onClick = (e) => {
+      setDisplayed((item) => calculate(item, e.target.innerText));
+    };
     const btn = ['AC', '+/-', '%', '÷', 7, 8, 9, 'x', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', ' ='];
     let { items } = this.state;
     items = btn.map((item) => (
-      <button type="button" className="keypadBtn" key={Math.random().toString(36).toUpperCase().substring(0, 5)}>
+      <button type="button" onClick={onClick} className="keypadBtn" key={Math.random().toString(36).toUpperCase().substring(0, 5)}>
         {item}
       </button>
     ));
+
+    const { total, next, operation } = displayed;
 
     return (
       <div className="boxCal">
         <div className="result">
           {total}
+          {operation}
+          {next}
         </div>
         <div className="keypadBtns">{items}</div>
       </div>
